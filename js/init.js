dc81a6f2cab2298f096f59a7ac71df77b9558802
Rakesh Sharma <raks437@users.noreply.github.com>
2015-04-21 13:30:14 +0530

    updated the timer

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 //Hook up the tweet display
 
 $(document).ready(function() {
-	$("#countdown").countdown("2015/02/14",function(e){
+	$("#countdown").countdown("2015/04/27",function(e){
 		//$(this).text(e.strftime(" %D days %H hours %M minutes %S seconds"));
 		$(".days").text(e.strftime("%D"));
 		$(".hours").text(e.strftime("%H"));
