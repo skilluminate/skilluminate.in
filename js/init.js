2d1ef5507d889590b44a94c2bea2814f459358c1
Rakesh Sharma <raks437@users.noreply.github.com>
2015-05-03 13:32:19 +0530

    updated date

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 //Hook up the tweet display
 
 $(document).ready(function() {
-	$("#countdown").countdown("2015/05/03",function(e){
+	$("#countdown").countdown("2015/05/05",function(e){
 		//$(this).text(e.strftime(" %D days %H hours %M minutes %S seconds"));
 		$(".days").text(e.strftime("%D"));
 		$(".hours").text(e.strftime("%H"));
